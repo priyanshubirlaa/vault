82473dc59ec360aeab463333dd0101f77f138398
Samyakjain2004 <118537845+Samyakjain2004@users.noreply.github.com>
2025-02-16 12:15:40 +0530

    Add files via upload

---
+++ vault_cloud/src/main/java/com/spring/vaidya/VaultCloudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.vaidya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultCloudApplication.class, args);
+	}
+
+}
